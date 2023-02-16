830b405c4149919c8b71a43a6c4be8dc9e6157b5
odith <musiimentaolix@gmail.com>
2023-02-16 13:39:10 -0800

    using printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/**
+ * main - a C program that uses printf to print a text to standard output.
+ * Return: 0
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
