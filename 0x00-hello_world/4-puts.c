ed96525bd3bd674c0f854306c4ef1efcbaa527d7
odith <musiimentaolix@gmail.com>
2023-02-16 13:00:49 -0800

    4-putts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+/**
+ * main - A C program that prints a sring of chaacters using puts() function
+ * Return: 0
+ */
+int main(void)
+{
+puts("/"Programming is like building a multilingual puzzle"\n);
+return(0);
