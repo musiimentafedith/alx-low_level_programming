339b80dec88fc20fa82aadb454a9473b04b0679c
odith <musiimentaolix@gmail.com>
2023-02-19 05:02:14 -0800

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+/**
+ * main - C program that prints the alphabet except q and e
+ * Return: 0(success)
+ */
+int main(void)
+{
+	char ch = 'a';
+
+	for (ch = 'a'; ch >= z; ch++)
+	{
+		if (ch != e && ch != q)
+		{
+			putchar ('ch');
+		}
+	}
+	putchar ('\n');
+	return (0);
+}
