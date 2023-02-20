1041985a6064bf43e9c10794fb3982c9dac1670e
odith <musiimentaolix@gmail.com>
2023-02-19 18:35:36 -0800

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+/**
+ * main - C program that prints lowercase alphabet in reverse order
+ * Return: 0
+ */
+
+int main(void)
+{
+	char ch;
+
+	for (ch = 'z'; ch >= 'a'; ch--)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
