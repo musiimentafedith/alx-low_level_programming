d3fa2b27fb8e22fa8b8e7cd8e48d734e5421df53
odith <musiimentaolix@gmail.com>
2023-02-19 17:49:56 -0800

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+/**
+ * main - C program to print alphabets both in lower and upper cases
+ * Return: 0
+ */
+
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('toupper(ch)');
+	ptchar('\n');
+	return (0);
+}
