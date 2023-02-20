87c7753f03c8e5793cdb3544b04f188def0d553f
odith <musiimentaolix@gmail.com>
2023-02-20 11:46:57 -0800

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,1 +1,30 @@
+#include<stdio.h>
 
+/**
+ * main - C program that prints  different possible combination of two digits
+ * Return: 0
+ */
+
+int main(void)
+{
+	int x, y;
+
+	for (x = 48; x <= 56; x++)
+	{
+		for (y = 49; y <= 57; y++)
+		{
+			if (y > x)
+			{
+				putchar(x);
+				putchar(y);
+				if (x != 56 || y != 57)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
