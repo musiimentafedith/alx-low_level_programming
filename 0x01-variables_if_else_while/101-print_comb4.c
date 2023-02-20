96bcede4c96a6deb7c0d9b453d53c89fcef0eb59
odith <musiimentaolix@gmail.com>
2023-02-20 11:29:35 -0800

    101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+
+/**
+ * main - C program to print posible combinations of three digit numbers
+ * Return: 0
+ */
+
+int main(void)
+{
+	int x, y, z;
+
+	for (x = 48; x <= 55; x++)
+	{
+		for (y = 49; y <= 56; y++)
+		{
+			for (z = 50; z <= 57; z++)
+			{
+				putchar(x);
+				putchar(y);
+				putchar(z);
+				if (x != 55 || y != 56)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
