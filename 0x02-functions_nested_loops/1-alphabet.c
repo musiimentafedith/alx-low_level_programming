5cc77c68003d15b8443a542c9018693dec88d81b
odith <musiimentaolix@gmail.com>
2023-02-21 06:03:50 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print alphabate in lowercase followed by a new line
+ * Return: 0
+ */
+
+void print_alphabet(void);
+{
+	char x;
+
+	for (x = 'a'; x <= 'z'; x++)
+	{
+		_putchar(x);
+	}
+	_putchar('\n');
+	return (0);
+}
