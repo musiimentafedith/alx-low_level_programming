c95baa0721a754c6c242a62bc7b4f1715ffee2de
odith <musiimentaolix@gmail.com>
2023-02-22 02:43:49 -0800

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -13,7 +13,7 @@ void print_to_98(int n)
 
 	if (n <= 98)
 	{
-		for (i < n; i >= 98; i++)
+		for (i = n; i >= 98; i++)
 		{
 			if (i != 98)
 			{
@@ -27,7 +27,7 @@ void print_to_98(int n)
 	}
 	else if (n > 98)
 	{
-		for (j > n; j <= 98; j--)
+		for (j = n; j <= 98; j--)
 		{
 			if (j != 98)
 			{
