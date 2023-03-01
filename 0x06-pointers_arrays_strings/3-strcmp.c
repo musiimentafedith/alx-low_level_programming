0d4b6c37b1f1d66b17eb3c89aed01447739f45f9
odith <musiimentaolix@gmail.com>
2023-03-01 12:30:34 -0800

    3-strcmp.c

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strcmp - compairs two strings
+ * @s1: string
+ * @s2: string
+ * Return: 0 if the strings are the same, else si - s2
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i, j;
+
+	j = 0;
+	for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
+	{
+		if (s1[i] != s2[i])
+		{
+			j = i;
+			break
+		}
+	}
+	if (j == 1)
+	{
+		return (s1 - s2);
+	}
+	else
+		return (0);
+}
