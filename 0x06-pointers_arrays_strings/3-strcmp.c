eeea8c2ed35d9c57c10f65012a5e9b487f838eb7
odith <musiimentaolix@gmail.com>
2023-03-02 01:48:50 -0800

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,21 +9,14 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int i, j;
+	int i;
 
-	j = 0;
 	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
 	{
 		if (s1[i] != s2[i])
 		{
-			j = 1;
-			break
+			return (s[i] - s2[i]);
 		}
 	}
-	if (j == 1)
-	{
-		return (s[i] - s2[i]);
-	}
-	else
-		return (0);
+	return (0);
 }
