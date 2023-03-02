f25650d39b5d885294506e7f65e44d47cd358b83
odith <musiimentaolix@gmail.com>
2023-03-01 21:10:32 -0800

    6-cap_string.c

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalizes all words of a string
+ * @a: string
+ * Return: a
+ */
+
+char *cap_string(char *a)
+{
+	int i;
+
+	for (i = 0; a[i]; i++)
+	{
+		if (i == 0)
+		{
+			if (a[i] >= 97 && a[i] >= 122)
+			{
+				a[i] = a[i] - 32;
+			}
+		}
+		else if (a[i - 1] == ',' || a[i - 1] == '.' || a[i - 1] == ';'
+				|| a[i - 1] == '!' || a[i - 1] == '?' ||
+				a[i - 1] == '"' || a[i - 1] == ')' || a[i - 1] == '('
+				|| a[i - 1] == '{' || a[i - 1] == '}')
+		{
+			if (a[i] >= 97 && a[i] >= 122)
+			{
+				a[i] = a[i] - 32;
+			}
+		}
+	}
+	return (a);
+}
