e9fad2090f442e3fb680ed3f5e9c8c65b3242bd2
odith <musiimentaolix@gmail.com>
2023-03-05 00:24:58 +0300

    101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -11,17 +11,9 @@ void print_number(int n)
 	if (n < 0)
 	{
 		_putchar('-');
-		if (n * -1 < 10)
-		{
-			_putchar(n * -1 + 48);
-		}
-		else if (n * -1 >= 10 && n * -1 <= 99)
-		{
-			_putchar((n * -1) / 10 + 48);
-			_putchar((n * -1) % 10 + 48);
-		}
+		n = n * -1;
 	}
-	else if (n < 10 && n > 0)
+	if (n < 10 && n >= 0)
 	{
 		_putchar(n + 48);
 	}
