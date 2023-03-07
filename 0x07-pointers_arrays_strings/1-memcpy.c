718f253bbb271047201d68d416a8ec1618c09a0c
odith <musiimentaolix@gmail.com>
2023-03-07 03:45:00 +0300

    1-memcpy.c

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _memcpy - copies memory area
+ * @dest: destination array
+ * @src: source array
+ * @n: number of bytes to copy
+ * Return: dest
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	while (n)
+	{
+		*(dest++) = *(src++);
+	}
+	return (dest);
+}
