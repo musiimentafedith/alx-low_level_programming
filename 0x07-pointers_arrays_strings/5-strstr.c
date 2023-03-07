6599a7813498568dd51b2701058d6829464d06ea
odith <musiimentaolix@gmail.com>
2023-03-07 02:27:03 -0800

    5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -12,7 +12,7 @@ char *_strstr(char *haystack, char *needle)
 
 	for (i = 0; haystack[i] != '\0'; i++)
 	{
-		if (haystack[i] == needle[0] && needle[0] != '\0')
+		if (haystack[i] == needle[0])
 		{
 			flag = 0;
 			for (j = 0; needle[j] != '\0'; j++)
@@ -23,7 +23,7 @@ char *_strstr(char *haystack, char *needle)
 					break;
 				}
 			}
-			if (flag == 0)
+			if (flag == 0 || needle[j] = '\0')
 			{
 				return (&haystack[i]);
 			}
