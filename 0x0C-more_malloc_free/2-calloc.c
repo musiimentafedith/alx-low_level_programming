fe861c5eaa07354ec9f11d11188b850fd82656b6
odith <musiimentaolix@gmail.com>
2023-03-19 11:28:27 +0300

    2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,45 +1,38 @@
+#include <stdlib.h>
 #include "main.h"
-#include<stdlib.h>
 
 /**
- * mset - sets memory to an int provided
- * @s: pointer
- * @x: int to set
- * @n: size of elements
- * Return: pointer
+ * *_memset - fills memory with a constant byte
+ * @s: memory area to be filled
+ * @b: char to copy
+ * @n: number of times to copy b
+ * Return: pointer to the memory area s
  */
-
-char *mset(char *s, char x, unsigned int n)
+char *mset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
 
 	for (i = 0; i < n; i++)
 	{
-		s[i] = x;
+		s[i] = b;
 	}
 	return (s);
 }
-
 /**
- * _calloc - allocates memory for an array, using malloc
- * @nmemb: number of elements
- * @size: size each element
- * Return: pointer to first allocated memory block
+ * *_calloc - allocates memory for an array
+ * @nmemb: number of elements in the array
+ * @size: size of each element
+ * Return: pointer to allocated memory
  */
-
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	unsigned int i;
 	char *ptr;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
-	ptr = malloc(nmemb * size);
+	ptr = malloc(size * nmemb);
 	if (ptr == NULL)
-	{
 		return (NULL);
-	}
 	mset(ptr, 0, nmemb * size);
 	return (ptr);
 }
-
