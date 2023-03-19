0943ad1ef96d27438dcedc72dd932cbda381790a
odith <musiimentaolix@gmail.com>
2023-03-19 11:30:36 +0300

    2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -2,7 +2,7 @@
 #include "main.h"
 
 /**
- * *_memset - fills memory with a constant byte
+ * *mset - fills memory with a constant byte
  * @s: memory area to be filled
  * @b: char to copy
  * @n: number of times to copy b
