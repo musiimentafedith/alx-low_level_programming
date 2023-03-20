2b618e9ed4e0f6cae799fb195e058baca304e554
odith <musiimentaolix@gmail.com>
2023-03-21 00:12:22 +0300

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - holds the dos details
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owners name
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
