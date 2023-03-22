593c7e0b1a47d0c7b958cbb0164a8f4e62521ac7
odith <musiimentaolix@gmail.com>
2023-03-22 17:30:34 +0300

    0-print_name.c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_name - prints a name
+ * @name: character string
+ * @f: function pointer
+ * Return: void
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	(*f)(name);
+}
