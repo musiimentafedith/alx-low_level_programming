d2daac1d123f34d869d9e069aaba09c800ee3afd
odith <musiimentaolix@gmail.com>
2023-03-30 23:30:53 +0300

    0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -11,16 +11,18 @@
 size_t print_list(const list_t *h)
 {
 	unsigned int count = 0;
-	list_t ptr = NULL;
 
 	if (h == NULL)
 		return (0);
-	prt = h;
-	while (ptr != NULL)
+	while (h != NULL)
 	{
-		printf("[%u] %s", ptr->len, ptr->str);
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)\n")
+		}
+		printf("[%u] %s\n", ptr->len, ptr->str);
 		count++;
-		ptr = ptr->next;
+		h = h->next;
 	}
 	return (count);
 }
