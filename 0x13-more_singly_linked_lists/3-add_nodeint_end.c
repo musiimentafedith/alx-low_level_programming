ad4b113b32a06fdde7a4656abc5101b1e6405d53
odith <musiimentaolix@gmail.com>
2023-04-04 06:08:21 +0300

    3

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -23,11 +23,14 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	{
 		*head = ptr;
 	}
-	temp = *head;
-	while (temp->next != NULL)
+	else
 	{
-		temp = temp->next;
+		temp = *head;
+		while (temp->next != NULL)
+		{
+			temp = temp->next;
+		}
+		temp->next = ptr;
 	}
-	temp->next = ptr;
 	return (ptr);
 }
