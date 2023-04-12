c30c832d168957040c4c7b3e3d35bb0036ca7824
odith <musiimentaolix@gmail.com>
2023-04-12 08:08:36 +0300

    1-create_file.c

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,27 @@
+#include"main.h"
+
+/**
+ * create_file - creates a file
+ * @filename: name of file to be created
+ * @text_content: content to be written
+ * Return: 1 on success, -1 on failure
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+
+	if (filename == NULL)
+		return (-1);
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	if (fd == -1)
+	{
+		return (-1);
+	}
+	if (text_content != NULL)
+	{
+		write(fd, text_content, strlen(text_content));
+	}
+	close(fd);
+	return (1);
+}
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,6 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
+
 #include<sys/types.h>
+#include<stdio.h>
+#include<unistd.h>
+#include<fcntl.h>
+#include<string.h>
+#include<stdlib.h>
 
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
