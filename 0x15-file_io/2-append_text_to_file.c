5fe3a4637600ef871ef8408ff57382ed730b08ab
odith <musiimentaolix@gmail.com>
2023-04-29 19:01:23 +0300

    task 2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -13,7 +13,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	int i = 0;
 	int w = 0;
 
-	if (filename == NULL || text_content == NULL)
+	if (filename == NULL)
 	{
 		return (-1);
 	}
