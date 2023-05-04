a68850eda9abd5a03ac61693c3a419bdd7067c9c
odith <musiimentaolix@gmail.com>
2023-05-04 17:18:46 +0300

    task 0

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,22 @@
+#include"main.h"
+
+/**
+ * binary_to_unit - converst a binary string to unsigned int
+ * @b: pointer to binary string
+ * Return: unsigned int or 0
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	int i, d_value = 0;
+
+	if (b == NULL)
+		return (0);
+	for (i = 0; b[i] != '\0'; i++)
+	{
+		if (b[i] < '0' || b[i] > '1')
+			return (0);
+		d_value = 2 * d_value + (b[i] - '0');
+	}
+	return (d_value);
+}
