4c4ad22c3e197dd78836538dea3783cd83b7ad29
odith <musiimentaolix@gmail.com>
2023-05-08 21:26:38 +0300

    task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -29,9 +29,13 @@ int create_file(const char *filename, char *text_content)
 	file_d = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 	if (file_d == -1)
 		return (-1);
-	len = _strlen(text_content);
-	written = write(file_d, text_content, len);
-	if (written == -1)
-		return (-1);
+	if (text_content != NULL)
+	{
+		len = _strlen(text_content);
+		written = write(file_d, text_content, len);
+		if (written == -1)
+			return (-1);
+	}
+	close(file_d);
 	return (1);
 }
