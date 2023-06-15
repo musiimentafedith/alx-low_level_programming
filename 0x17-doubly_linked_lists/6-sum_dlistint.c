743a990b3d1cf761396535a9d59cf5d4d9296996
odith <musiimentaolix@gmail.com>
2023-06-15 21:54:17 +0300

    fixxed betty warnings

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -1,7 +1,7 @@
 #include"lists.h"
 
 /**
- * sum_dlistint - returns the sum of all the data (n) of a dlistint_t linked list
+ * sum_dlistint - returns the sum of all the data (n) of a dlistint_t
  * @head: pointer to the head node
  * Return: sum of all the data (n) or 0 for empty list
  */
