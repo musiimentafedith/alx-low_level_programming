b62dab9f8eb1c4513fb231420e86f156e92b99f8
odith <musiimentaolix@gmail.com>
2023-06-15 23:48:08 +0300

    task 7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,4 +1,23 @@
 #include"lists.h"
+#include"lists.h"
+/**
+ * dlistint_len - returns the number of elements in a linked dlistint_t list
+ * @h: pointer to the head node
+ * Return: number of elements in a linked dlistint_t list
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	const dlistint_t *temp = h;
+	size_t count = 0;
+
+	while (temp != NULL)
+	{
+		count += 1;
+		temp = temp->next;
+	}
+	return (count);
+}
 
 /**
  * insert_dnodeint_at_index - inserts a new node at a given position
